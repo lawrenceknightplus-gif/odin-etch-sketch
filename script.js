d4b5290c77bb661857d8a975f01b280e39995671
lawrence <lawrenceknightplus@gmail.com>
2026-01-13 17:03:57 +0000

    build : initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// get rows, cols and store in a veriable
+let rows = 16;
+let cols = 16;
+
+// func create grid cells
+
+function createCells(rows, cols) {
+  // get grid element
+  const grid = document.querySelector('.grid')
+  let gridSize = grid.clientWidth + grid.clientHeight
+  // for rows * cols increment and make new cells
+  for(let i = 0; i < (rows * cols); i++){
+  // create new div el
+  let cell = document.createElement('div')
+  // display new grid to user on webpage
+  cell.style.width = `${gridSize / cols}px`
+  cell.style.height = `${gridSize / rows}px`
+  cell.style.boxShadow = "0 0 1px black inset"
+
+  grid.append(cell)
+  }
+}
+
+createCells(rows, cols)
+
+// func change grid row & col size
+// display new grid
+
+// func change to random color cells
+// generate random color for each cell
+
+// func change to dark cells with that goes from light to dark
