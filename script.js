e40ce7660e214ac89aae6428a5fcf1503aa8303b
lawrence <lawrenceknightplus@gmail.com>
2026-01-13 17:36:25 +0000

    update : add hover functions

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 // get rows, cols and store in a veriable
-let rows = 16;
-let cols = 16;
+let rows = 40;
+let cols = 40;
+let cellHoverMode = "normal";
 
 // func create grid cells
 
 function createCells(rows, cols) {
   // get grid element
-  const grid = document.querySelector('.grid')
-  let gridSize = grid.clientWidth + grid.clientHeight
+  const grid = document.querySelector(".grid");
+  let gridSize = grid.clientWidth + grid.clientHeight;
   // for rows * cols increment and make new cells
-  for(let i = 0; i < (rows * cols); i++){
-  // create new div el
-  let cell = document.createElement('div')
-  // display new grid to user on webpage
-  cell.style.width = `${gridSize / cols}px`
-  cell.style.height = `${gridSize / rows}px`
-  cell.style.boxShadow = "0 0 1px black inset"
+  for (let i = 0; i < rows * cols; i++) {
+    // create new div el
+    let cell = document.createElement("div");
+    // display new grid to user on webpage
+    cell.style.width = `${gridSize / cols}px`;
+    cell.style.height = `${gridSize / rows}px`;
+    cell.addEventListener("mouseover", cellHover);
+    cell.addEventListener("mouseout", cellNoHover);
 
-  grid.append(cell)
+    grid.append(cell);
   }
 }
 
-createCells(rows, cols)
+createCells(rows, cols);
+
+function cellHover(e) {
+  switch (cellHoverMode) {
+    case "normal":
+      e.target.className = "hover";
+      break;
+  }
+}
+function cellNoHover(e) {
+  e.target.className = "cell";
+}
 
 // func change grid row & col size
 // display new grid
